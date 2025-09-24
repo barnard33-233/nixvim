06532cfc33de3b6136ab0a093f5875095989f42d
barnard33-233 <htang0203@gmail.com>
2025-09-24 16:29:21 +0800

    feat: disable auto-closing of "'" in .rs files

--- config/autoclose.nix
+++ config/autoclose.nix
@@ -2,4 +2,11 @@
   plugins.autoclose = {
     enable = true;
   };
+  extraConfigLua = ''
+    require("autoclose").setup({
+       keys = {
+          ["'"] = { escape = true, close = true, pair = "'", disabled_filetypes = {"rust"} },
+       },
+    })
+  '';
 }
